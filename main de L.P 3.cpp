102a06fa5da5c131d189c7342b6e365830fefdef
Mati9284 <matiaseest5@gmail.com>
2025-04-07 18:48:06 -0300

    Add files via upload

---
+++ main de L.P 3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float numero1, resultado;
+	cout << "Este programa calcula el perimetro de un cuadrado" << endl;
+	cout << "Ingrese lado del cuadrado" << endl;
+	cin >> numero1;
+	resultado= numero1*4;
+	cout << "El resultado del perimetro es:" << resultado << endl;
+	return 0;
+}
